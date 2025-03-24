690ea1d66a9f2a1e0c93c632685944bc10e5047d
Navyayalamat <you@example.com>
2025-03-24 23:49:14 +1100

    react hooks

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,25 +1,63 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import './Product.scss'
 import Card from './Card'
 
-
+ 
 const Product = () => {
+    let initialState = {
+        loading: false,
+        data: null,
+        error: false
+    }
+    const fetchInitialState = 'initial State';
+    const startState = 'start state';
+    const errorState = 'error state';
+
+    const dataReducer = (state, action) => {
+        debugger
+       if(action.type === fetchInitialState){
+        return {...state, loading : true, error: false}
+       }
+       else if(action.type === startState){
+        return {...state,  loading: false , data: action.response}
+       }
+       else if( action.type === errorState){
+        return {...state, error: action.response}
+       }
+    }
+
+
     const [productsData, setProductsData] = useState([]);
+    const[state, dispatch] = useReducer(dataReducer,initialState) 
+    // useEffect( ()=> {
+    //     getCustomersData();
+    // }, [])
+
      const getCustomersData = () => {
+        debugger
+        dispatch({type: fetchInitialState });
         axios
             .get("https://dummyjson.com/products")
-            .then(data => { console.log("sdsdsd",data) 
-                setProductsData(data.data.products)})
-            .catch(error => console.log(error));
+            .then(data => {  
+                console.log("data.data.products", data)
+                // setProductsData(data.data.products)
+                dispatch({type: startState, response:data.data.products })
+                console.log("state", state)
+             }
+            )
+            .catch(error =>  dispatch({type: errorState, response: error }) );
     };
-
+    
     getCustomersData();
         return(
-            <div className="products">
-              {productsData?.length > 0 && productsData.map(((product, index) => 
-                  (<Card  key={index} productData={product}/>)
-              ))}
+            <div>
+                <i className="fa fa-spinner fa-spin"></i>
+                <div className="products">
+                    {productsData?.length > 0 && productsData.map(((product, index) => 
+                        (<Card  key={index} productData={product}/>)
+                    ))}
+                </div>
             </div>
         )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CreateUser from './Create.jsx'
 import Header from './Header.jsx'
 import Product from './Product.jsx'
-import ProductList from './ProductList.jsx'
 import {BrowserRouter} from "react-router-dom";
 
 
@@ -16,7 +14,6 @@ createRoot(document.getElementById('root')).render(
       <App />
       <Header />
       <Product />
-      <ProductList/>
   </BrowserRouter>
    
   </StrictMode>,
